d80e37d29ddbf6ebd20bf6d74a240a0bb0f9913c
saantt <santiagomaring05@gmail.com>
2025-10-02 09:43:22 -0500

    Estructuracion del proyecto

---
+++ backend/src/main/java/co/edu/uniquindio/software/actividad/saludvital/SaludvitalApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.software.actividad.saludvital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludvitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaludvitalApplication.class, args);
+	}
+
+}
